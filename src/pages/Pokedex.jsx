7e1c7d16433c23b21445678ff30d37966550b275
SammLad <laurasofialadinopelaez@gmail.com>
2026-09-08 09:38:19 -0500

    feat: implementar CRUD del equipo Pokemon

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,7 +1,8 @@
 import { use, useState } from "react";
 import { buscarPokemon } from "../services/pokeApi";
+import { agregarAlEquipo } from "../services/equipoApi";
 
-function Pokedex() {
+function Pokedex({ onPokemonAgregado }) {
     const [busqueda, setBusqueda] = useState("");
     const [pokemon, setPokemon] = useState(null);
     const [error, setError] = useState("");
@@ -13,6 +14,29 @@ function Pokedex() {
             setPokemon(datos);
         } catch (error) {
             setPokemon(null);
+            setError(error.message);
+        }
+    };
+
+    const agregarPokemon = async () => {
+        if (!pokemon) {
+            return;
+        }
+
+        const nuevoPokemon = {
+            nombre: pokemon.name,
+            imagen: pokemon.sprites.front_default,
+            nivel: 1,
+            favorito: false
+        };
+
+        try {
+            await agregarAlEquipo(nuevoPokemon);
+            onPokemonAgregado();
+            alert(
+                `${pokemon.name} fue agregado al equipo`
+            );
+        } catch (error) {
             setError(error.message);
         }
     };
@@ -43,6 +67,10 @@ function Pokedex() {
 
                     <p>Altura: {pokemon.height}</p>
                     <p>Peso: {pokemon.weight}</p>
+
+                    <button onClick={agregarPokemon}>
+                        Agregar a mi equipo
+                    </button>
                 </article>
             )}
 
